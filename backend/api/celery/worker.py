ff8af88cec172708f3062cd9dc8ff896816590c3
Segfaul <ikaruzds@gmail.com>
2024-06-02 20:44:29 +0300

    revert celery task timings

--- backend/api/celery/worker.py
+++ backend/api/celery/worker.py
@@ -8,12 +8,12 @@ celery = Celery(__name__, broker=os.environ['CELERY_BROKER_URL'], include=['back
 celery.conf.beat_schedule = {
     'parse_dota-every-70': {
         'task': 'backend.api.celery.tasks.parse_opendota',
-        'schedule': crontab(minute='*/3.5'),
+        'schedule': crontab(minute='*/70'),
         'args': ()
     },
     'populate_db-every-30': {
         'task': 'backend.api.celery.tasks.populate_db',
-        'schedule': crontab(minute='*/1.5'),
+        'schedule': crontab(minute='*/30'),
         'args': ()
     },
 }
